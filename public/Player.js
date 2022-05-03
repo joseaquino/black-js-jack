4024f2f9734215a7a3dccb223532c835cac46412
Jose Aviles <aviles.yanes7@gmail.com>
2022-05-02 19:06:01 -0600

    double down functionality and split one have bee added

--- public/Player.js
+++ public/Player.js
@@ -11,6 +11,7 @@ export default class Player {
     this.playerType = playerType;
     this.nextCardToRender = 0;
     this.bet = 0;
+    this.splitedCards = false;
   }
 
   renderPlayer() {
@@ -20,11 +21,11 @@ export default class Player {
   playerHtml() {
     return `
     <div class="player" id="${this.name}">
+      <div class='player__bet-value'></div>
       <h2 class="player-heading">${this.name}</h2>
       <div class="card"></div>
       <div class='player__hand-value'></div>
-      <div class='player__bet-value'></div>
-    </div>
+      </div>
     
     `;
   }
@@ -47,6 +48,16 @@ export default class Player {
   renderBetValue() {}
 
   sumOfCards() {
+    let hand;
+
+    switch (true) {
+      case this.playerPlayedBothHands:
+        hand = this.secondHand;
+        break;
+      default:
+        hand = this.hand;
+    }
+
     let sumOfHand = 0;
 
     const [arrOfCardsWithFixedValues, arrOfCardsWithAlternateValues] =
@@ -69,6 +80,47 @@ export default class Player {
       sumOfHand += card.value;
     }
 
+    if (amountOfAces === 0) {
+      switch (true) {
+        case this.playerPlayedBothHands:
+          this.hand2Value = sumOfHand;
+          break;
+        case this.splitedCards:
+          this.hand1Value = sumOfHand;
+          break;
+        default:
+          this.handValue = sumOfHand;
+      }
+
+      return;
+    }
+    // debugger;
+    for (let card of arrOfCardsWithAlternateValues) {
+      if (card.faceDirection === "down") break;
+      if (sumOfHand + card.value > 21) {
+        sumOfHand = sumOfHand + card.alternateValue;
+        alternateValuesUsed++;
+      } else {
+        sumOfHand = sumOfHand + card.value;
+      }
+
+      if (sumOfHand > 21 && alternateValuesUsed < amountOfAces) {
+        sumOfHand -= amountReducedIfAceChangesTo1;
+        alternateValuesUsed++;
+      }
+    }
+
+    switch (true) {
+      case this.playerPlayedBothHands:
+        this.hand2Value = sumOfHand;
+        break;
+      case this.splitedCards:
+        this.hand1Value = sumOfHand;
+        break;
+      default:
+        this.handValue = sumOfHand;
+    }
+    //Anterior 
     if (amountOfAces !== 0) {
       for (let card of arrOfCardsWithAlternateValues) {
         if (card.faceDirection === "down") break;
@@ -84,6 +136,18 @@ export default class Player {
     return;
   }
 
+  playerHandValueHtml() {
+    switch (true) {
+      case this.playerPlayedBothHands:
+        return `${this.hand2Value}`;
+
+      case this.splitedCards:
+        return `${this.hand1Value}`;
+      default:
+        return `${this.handValue}`;
+    }
+  }
+
   renderHandValue() {
     const sumContainer = this.domElement.querySelector(".player__hand-value");
     sumContainer.innerHTML = this.handValue.toString();
@@ -94,6 +158,46 @@ export default class Player {
     this.renderNextCard();
     this.sumOfCards();
     this.renderHandValue();
+  }
+
+  receiveCardforSplittedHand(card) {
+    if (this.playerPlayedBothHands) {
+      this.secondHand.push(card);
+    } else {
+      this.hand.push(card);
+    }
+    this.renderCardWhenPlayerHasSplitted();
+    this.sumOfCards();
+    this.renderHandValueWhenPlayerHasSplitted();
+  }
+
+  // receiveCardforHand2(card) {
+  //   this.secondHand.push(card);
+  // }
+
+  renderHandValueWhenPlayerHasSplitted() {
+    const sumContainer = document.querySelector(
+      `#${this.name} .card.active .player__hand-value`
+    );
+    sumContainer.innerHTML = this.playerHandValueHtml();
+  }
+
+  renderCardWhenPlayerHasSplitted() {
+    let cardToRender;
+    if (this.playerPlayedBothHands) {
+      cardToRender = this.secondHand[this.nextCardToRenderHand2];
+    } else {
+      cardToRender = this.hand[this.nextCardToRenderHand1];
+    }
+    let htmlString = `<p>${cardToRender.number} of ${cardToRender.suit} <i class="${cardToRender.icon}  "></i></p>`;
+
+    const cardContainerElem = document.querySelector(".card.active");
+    cardContainerElem.insertAdjacentHTML("afterbegin", htmlString);
+    if (this.playerPlayedBothHands) {
+      this.nextCardToRenderHand2++;
+    } else {
+      this.nextCardToRenderHand1++;
+    }
   }
 
   renderNextCard() {
--- public/playerDecisionController.js
+++ public/playerDecisionController.js
@@ -1,4 +1,5 @@
 "use strict";
+import { sleepNow } from "./Helpers.js";
 
 export default class PlayerDecisionController {
   constructor(board) {
@@ -21,10 +22,19 @@ export default class PlayerDecisionController {
     this.stand = this.controlsContainerEle.querySelector(".btn--standing");
     this.split = this.controlsContainerEle.querySelector(".btn--splitting");
     this.double = this.controlsContainerEle.querySelector(".btn--doubling");
+    this.playerCotrols =
+      this.controlsContainerEle.querySelector(".player-controls");
 
     this.hit.addEventListener("click", this.hitting.bind(this));
     this.stand.addEventListener("click", this.nextPlayer.bind(this));
+    this.double.addEventListener("click", this.doublingDown);
+    this.split.addEventListener("click", this.splitting);
 
+    this.players = players;
+    this.amountOfPlayers = this.players.length;
+    this.generatorCardObject = generatorCardObject;
+    this.currentPlayerTurn = this.definePlayerTurn();
+    
     this.definePlayerTurn();
     this.activePlayer = this.players[this.currentPlayerTurn];
   }
@@ -38,7 +48,7 @@ export default class PlayerDecisionController {
     <button type='button' class='player-controls__btn btn--standing'> 
     Stand
     </button>
-    <button type='button' class='player-controls__btn btn--splitting'> 
+    <button type='button' class='player-controls__btn btn--splitting' disabled> 
     Split
     </button>  
     <button type='button' class='player-controls__btn btn--doubling'> 
@@ -52,7 +62,10 @@ export default class PlayerDecisionController {
     for (let player of this.players) {
       if (player.handValue === 21) this.currentPlayerTurn++;
       else {
-        this.activePlayer = this.players[this.currentPlayerTurn];
+        this.activePlayer = this.players[currentPlayerTurn];
+        if (this.haveSameValuePlayerCards()) {
+          this.split.removeAttribute("disabled");
+        }
         document
           .querySelector(`#${this.activePlayer.name} .card`)
           .classList.add("active");
@@ -65,7 +78,13 @@ export default class PlayerDecisionController {
     }
   }
 
-  hasPlayer21OrMore() {
+  haveSameValuePlayerCards() {
+    return this.activePlayer.hand[0].value === this.activePlayer.hand[0].value;
+  }
+
+  
+
+  hasPlayerMoreThan21() {
     return this.activePlayer.handValue >= 21;
   }
 
@@ -74,6 +93,7 @@ export default class PlayerDecisionController {
   }
 
   dealerPlay() {
+    this.double.setAttribute("disabled", "true");
     this.stand.setAttribute("disabled", "true");
     this.hit.setAttribute("disabled", "true");
     const dealer = this.players[this.amountOfPlayers - 1];
@@ -89,6 +109,42 @@ export default class PlayerDecisionController {
   }
 
   nextPlayer() {
+    if (this.activePlayer.playerPlayedBothHands)
+      document
+        .querySelector(`#${this.activePlayer.name} .card`)
+        .lastElementChild.classList.remove("active");
+
+    if (
+      this.activePlayer.splitedCards &&
+      !this.activePlayer.playerPlayedBothHands
+    ) {
+      document
+        .querySelector(`#${this.activePlayer.name} .card`)
+        .firstElementChild.classList.remove("active");
+      document
+        .querySelector(`#${this.activePlayer.name} .card`)
+        .lastElementChild.classList.add("active");
+      this.activePlayer.playerPlayedBothHands = true;
+      return;
+    }
+    this.double.removeAttribute("disabled");
+    this.split.removeAttribute("disabled");
+    document
+      .querySelector(`#${this.activePlayer.name}`)
+      .querySelector(".card")
+      .classList.remove("active");
+
+    this.currentPlayerTurn++;
+    this.activePlayer = this.players[this.currentPlayerTurn];
+    if (this.hasPlayer21() && !this.isDealerTurn()) this.currentPlayerTurn++;
+    document
+      .querySelector(`#${this.activePlayer.name}`)
+      .querySelector(".card")
+      .classList.add("active");
+
+      //revisar
+
+    nextPlayer() {
     this.activePlayer.removeFocus();
     this.currentPlayerTurn++;
     this.activePlayer = this.players[this.currentPlayerTurn];
@@ -106,6 +162,52 @@ export default class PlayerDecisionController {
     return;
   }
 
+  dealCard() {
+    return this.generatorCardObject.next().value;
+  }
+
+  doublingDown = async () => {
+    const bettingCont = document.querySelector(
+      `#${this.activePlayer.name} .player__bet-value`
+    );
+    const [betValueStr] = bettingCont.textContent.split(" ").slice(1);
+    const betValueNum = betValueStr * 1; //converting string to number
+    const remainingAfterDoubling = this.activePlayer.pot - betValueNum;
+
+    if (remainingAfterDoubling < 0) {
+      this.playerCotrols.insertAdjacentHTML("beforeend", "not enough funds!!!");
+      this.double.setAttribute("disabled", "true");
+      await sleepNow(1000);
+      this.playerCotrols.lastChild.remove();
+      this.double.setAttribute("disabled", "true");
+      return;
+    }
+
+    this.activePlayer.pot -= betValueNum;
+    bettingCont.innerHTML = `Betting: ${betValueNum * 2}`; //doubling betValueNum;
+    this.double.setAttribute("disabled", "true");
+
+    this.hitting(true);
+    this.nextPlayer();
+  };
+
+  hitting(fromDoubling) {
+    this.double.setAttribute("disabled", "true");
+    this.split.setAttribute("disabled", "true");
+    const card = this.dealCard();
+    if (this.activePlayer.splitedCards) {
+      this.activePlayer.receiveCardforSplittedHand(card);
+      return;
+    }
+
+    this.activePlayer.receiveCard(card);
+
+    if (
+      (this.hasPlayerMoreThan21() || this.hasPlayer21()) &&
+      !this.isDealerTurn() &&
+      fromDoubling !== true
+    )
+    //left
   hitting() {
     const card = this.board.cardDeck.takeCard();
     this.activePlayer.receiveCard(card);
@@ -114,4 +216,97 @@ export default class PlayerDecisionController {
     }
     return;
   }
+
+  standing() {
+    this.nextPlayer();
+  }
+
+  splitting = async () => {
+    const enoughFoundsforSplitting = await this.betDisplaysplitting();
+    if (!enoughFoundsforSplitting) return;
+
+    this.double.setAttribute("disabled", "true");
+
+    this.activePlayer.splitedCards = true;
+    this.fromSplittig = true;
+
+    this.removeValueOfOriginalHandWhenSplitting();
+    this.cardSplitting();
+    this.setValueOfSplittedHands();
+  };
+
+  cardSplitting = () => {
+    const playerCont = document
+      .querySelector(`#${this.activePlayer.name}`)
+      .querySelector(".card");
+    const cardContainerElem = document.querySelector(".card.active");
+    const firstPlayerCard = cardContainerElem.lastChild;
+    const secondPlayerCard = cardContainerElem.firstChild;
+
+    //definiendo 2 manos para el jugador que hace el split
+    this.activePlayer.secondHand = this.activePlayer.hand.splice(-1);
+    this.activePlayer.nextCardToRenderHand1 = 1;
+    this.activePlayer.nextCardToRenderHand2 = 1;
+    this.activePlayer.hand1Value = this.activePlayer.hand[0].value;
+    this.activePlayer.hand2Value = this.activePlayer.secondHand[0].value;
+
+    document
+      .querySelector(`#${this.activePlayer.name}`)
+      .querySelector(".card")
+      .classList.remove("active");
+
+    playerCont.innerHTML = `
+    <div class="card active">
+    ${firstPlayerCard.outerHTML}
+    <div class='player__hand-value'></div>
+    </div>
+    <div class="card">
+    ${secondPlayerCard.outerHTML}
+    <div class='player__hand-value'></div>
+    </div>
+    `;
+  };
+
+  betDisplaysplitting = async () => {
+    const bettingCont = document.querySelector(
+      `#${this.activePlayer.name} .player__bet-value`
+    );
+    const [betValueStr] = bettingCont.textContent.split(" ").slice(1);
+    const betValueNum = betValueStr * 1; //converting string to number
+    const remainingAfterDoubling = this.activePlayer.pot - betValueNum;
+    this.split.setAttribute("disabled", "true");
+
+    if (remainingAfterDoubling < 0) {
+      this.playerCotrols.insertAdjacentHTML("beforeend", "not enough funds!!!");
+
+      await sleepNow(1000);
+      this.playerCotrols.lastChild.remove();
+      // this.double.setAttribute("disabled", "true");
+      return false;
+    }
+
+    this.activePlayer.pot -= betValueNum;
+    bettingCont.innerHTML = `<div>Betting hand 1: ${betValueNum}</div>
+                             <div>Betting hand 2: ${betValueNum}</div>`;
+
+    return true;
+  };
+
+  removeValueOfOriginalHandWhenSplitting = () => {
+    const sumContainer = document.querySelector(
+      `#${this.activePlayer.name} .player__hand-value`
+    );
+    sumContainer.innerHTML = "";
+  };
+
+  setValueOfSplittedHands = () => {
+    const sumContainerHand1 = document.querySelector(
+      `#${this.activePlayer.name} .card.active .player__hand-value`
+    );
+    sumContainerHand1.innerHTML = this.activePlayer.hand1Value;
+    const sumContainerHand2 = document
+      .querySelector(`#${this.activePlayer.name} .card`)
+      .lastElementChild.querySelector(".player__hand-value");
+    sumContainerHand2.innerHTML = this.activePlayer.hand2Value;
+  };
 }
